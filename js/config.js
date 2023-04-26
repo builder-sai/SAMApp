b68d8ceba7bf3737827ddc638e9e8ea1112bf370
Saif Al <saifalzobaydee@hotmail.com>
2023-04-26 10:30:47 +0200

    updated config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '	eu-north-1_ycOadocAo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '53d0fv8rvquau7eafugi5jdt95', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://saifsportal.auth.eu-north-1.amazoncognito.com' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
