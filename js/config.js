b9bd45a0bc5c7d66a4f7e20dcd4c444735c0821a
Saif Al <saifalzobaydee@hotmail.com>
2023-04-26 12:34:23 +0200

    updated to yet another pool

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-north-1_ycOadocAo', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '8leipkvpg2m6a6718idgcril5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'eu-north-1_zeavfrRNH', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22o0urnaoj3evrq9dt8q7uhlqg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
